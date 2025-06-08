56dd38ae901a69af07223115911ddba40d4f6e20
Mark Shamanskii <darkweid713@gmail.com>
2025-06-08 19:12:51 +0500

    Chore: Remove unused DatabaseManager classes and clean up imports in __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,6 +1,1 @@
-from .managers import (DatabaseManager, TestingManager, UserProgressManager, UserManager, NewWordsExerciseManager,
-                       UserWordsLearningManager, DailyStatisticsManager)
 from .init import init_db
-
-__all__ = ['DatabaseManager', 'TestingManager', 'UserProgressManager', 'UserManager', 'NewWordsExerciseManager',
-           'UserWordsLearningManager', 'DailyStatisticsManager']
